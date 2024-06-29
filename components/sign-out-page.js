6147d28c72cb0ec9b7314a63bac813d8b91ae38d
Joel <varghesejoel6@gmail.com>
2024-06-29 16:50:19 +0530

    fixed link error

--- components/sign-out-page.js
+++ components/sign-out-page.js
@@ -7,7 +7,7 @@ export default function SignedOutPage({ handleSignin }) {
     <div className="container p-4 flex flex-col lg:flex-row justify-between items-center relative w-full h-full">
       <div className="flex flex-col items-center justify-center lg:items-start z-10 min-w-[50%] md:mt-8">
         <p className="text-4xl lg:text-5xl xl:text-6xl font-semibold text-center lg:text-left max-w-[350px] lg:max-w-[600px] leading-tight">
-          Send ETH to Wallet
+          Send Sepolia to any Wallet
         </p>
         <p className="text-center lg:text-left mt-2 opacity-80 max-w-[500px] lg:max-w-[450px]">
           connect your wallet and send testnet ETH to any wallet
